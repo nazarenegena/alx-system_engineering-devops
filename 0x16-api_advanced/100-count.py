6557a08078c79194b120d8a73b800d1bd9576d41
nazarenegena <genarene96@gmail.com>
2023-08-08 12:54:43 +0000

    refactored code

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,45 +1,48 @@
 #!/usr/bin/python3
 """getting the subs"""
-from requests import get
-from sys import argv
-
-toplist = []
-after = None
+import requests
 
 
-def counting_all(toplist, word_list):
-    counting_dict = {word.lower(): 0 for word in word_list}
-    for title in toplist:
-        words = title.split(' ')
-        for word in words:
-            if counting_dict.get(word) is not None:
-                counting_dict[word] += 1
+def count_words(subreddit, word_list, instances={}, after="", count=0):
+    """Printing counts of given words in hot posts."""
+    hot_url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    header = {
+        "User-Agent": "alx:lin.api.adv:v1.0.0 (by /u/gena)"
+    }
+    param_list = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    res = requests.get(hot_url, headers=header, params=param_list,
+                            allow_redirects=False)
+    try:
+        results = res.json()
+        if res.status_code == 404:
+            raise Exception
+    except Exception:
+        print("")
+        return
 
-    for key in sorted(counting_dict, key=counting_dict.get, reverse=True):
-        if counting_dict.get(key):
-            for thing in word_list:
-                if key == thing.lower():
-                    print("{}: {}".format(thing, counting_dict[key]))
+    results = results.get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        title = c.get("data").get("title").lower().split()
+        for word in word_list:
+            if word.lower() in title:
+                hot_times = len([t for t in title if t == word.lower()])
+                if instances.get(word) is None:
+                    instances[word] = hot_times
+                else:
+                    instances[word] += hot_times
 
-
-def count_words(subreddit, word_list):
-    global toplist
-    global after
-    """get subs"""
-    head = {'User-Agent': 'alx:lin.api.adv:v1.0.0 (by /u/gena)'}
-    if after:
-        response = get('https://www.reddit.com/r/{}/hot.json?after={}'.format(
-            subreddit, after), headers=head).json().get('data')
+    if after is None:
+        if len(instances) == 0:
+            print("")
+            return
+        instances = sorted(instances.items(), key=lambda kv: (-kv[1], kv[0]))
+        [print("{}: {}".format(k, v)) for k, v in instances]
     else:
-        response = get('https://www.reddit.com/r/{}/hot.json'.format(
-            subreddit), headers=head).json().get('data')
-    toplist += [dic.get('data').get('title').lower()
-                for dic in response.get('children')]
-    after = response.get('after')
-    if after:
-        return count_words(subreddit, word_list)
-    return counting_all(toplist, word_list)
+        count_words(subreddit, word_list, instances, after, count)
 
-
-if __name__ == "__main__":
-    count_words(argv[1], argv[2].split(' '))
